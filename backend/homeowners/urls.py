d43d16253dc88d49cd3ee62a99edff09023f75a1
Andreas <andreasneacsu@gmail.com>
2024-04-02 15:51:08 -0700

    added urls for scored plans

--- backend/homeowners/urls.py
+++ backend/homeowners/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView, LoginView, UserViewSet, MortgageInsuranceViewSet, LawnViewSet, InteriorViewSet, InternetViewSet, PhoneViewSet, LawnServicePlanViewSet, InteriorServicePlanViewSet, InternetServicePlanViewSet, PhoneServicePlanViewSet, logout_view, PropertyViewSet, ScoredLawnPlans
+from .views import RegisterView, LoginView, UserViewSet, MortgageInsuranceViewSet, LawnViewSet, InteriorViewSet, InternetViewSet, PhoneViewSet, LawnServicePlanViewSet, InteriorServicePlanViewSet, InternetServicePlanViewSet, PhoneServicePlanViewSet, logout_view, PropertyViewSet, ScoredLawnPlans, ScoredInteriorPlans, ScoredInternetPlans, ScoredPhonePlans
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -20,6 +20,9 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register/', RegisterView.as_view(), name='register'),
     path('scored-lawn-plans/<int:property_id>/', ScoredLawnPlans.as_view(), name='scored-lawn-plans'),
+    path('scored-interior-plans/<int:property_id>/', ScoredInteriorPlans.as_view(), name='scored-interior-plans'),
+    path('scored-internet-plans/<int:property_id>/', ScoredInternetPlans.as_view(), name='scored-internet-plans'),
+    path('scored-phone-plans/<int:property_id>/', ScoredPhonePlans.as_view(), name='scored-phone-plans'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
 
